7ad556f02223b7806e5292fcf98fe3ef1fd57b8f
MistoGank <nikipetkovv00@gmail.com>
2025-10-10 15:03:08 +0200

    update

--- shop/backend/forms/db/product/db_product_select.php
+++ shop/backend/forms/db/product/db_product_select.php
@@ -8,24 +8,46 @@
 // Debug 
 print_r($_POST); 
 // Variables
-$id_product = $_POST['id_product'];
+$product_output = "No product selected or found";
 
-// Query
-$sql = "
-SELECT * FROM products WHERE id_product = $id_product;
-";
+// Before starting the query, check If the variable was sended and that the variabel is not empty
 
-// Connection 
-include($_SERVER['DOCUMENT_ROOT'].'/student022/shop/backend/config/connection.php');
-//msqli_query
-if (mysqli_query($conn,$sql)){
-
-    $result = mysqli_fetch_assoc(mysqli_query($conn,$sql));
-    foreach ($result as $product){
-        echo "<p>$result.['id_product']</p>";
-    };
+if (!isset($_POST['id_product']) || empty($_POST['id_product'])){
+    $product_output = "Error: Product ID is missing";
 }else{
-     echo "<p>Error al insertar el producto: " . mysqli_error($conn) . "</p>";
+    // Open connection
+    include($_SERVER['DOCUMENT_ROOT'].'/student022/shop/backend/config/connection.php');
+    // escape the string to avoid mysql injection
+    $id_product = mysqli_real_escape_string($conn, $_POST['id_product']);
+
+    // Query
+    $sql = "SELECT * FROM products WHERE id_product = '$id_product'";
+
+    // Execute the query
+    $query_result = mysqli_query($conn,$sql);
+
+    // Check if the query exists and if there was rows affected
+    if ($query_result){
+        // Check if any row where returned
+        if (mysqli_num_rows($query_result)>0){
+            // Loop and return formatted result
+            while ($row = mysqli_fetch_assoc($query_result)){
+                // Format the output
+                printf("<p>"."Product ID: ".$row['id_product']."<p>");
+                printf("<p>"."Product Name: ".$row['product_name']."<p>");
+                printf("<p>"."Product Price: ".$row['price']."€"."<p>");
+            }
+        }else{
+            $product_output = "Product with ID $id_product not found.";
+        }
+    }else{
+        // Error on query execution
+        $product_output = "Database Error: " . mysqli_error($conn);
+    }
+    // Free the result
+    mysqli_free_result($query_result);
+    // Close connection
+    mysqli_close($conn);
 }
 ?>
 <!-- Footer -->
