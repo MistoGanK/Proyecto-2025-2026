3db20da0963224346d34fb9c82c0cd7ea5f71ba9
MistoGank <nikipetkovv00@gmail.com>
2025-10-20 12:49:58 +0200

    update

--- shop/backend/admin/orders.php
+++
@@ -1,7 +0,0 @@
-<?php 
-?>
-    <?php include($_SERVER['DOCUMENT_ROOT'].'/student022/shop/backend/header.php'); ?>
-
-    <p>Hola bbs</p>
-    
-    <?php include($_SERVER['DOCUMENT_ROOT'].'/student022/shop/backend/footer.php'); ?>
--- shop/backend/config/connection.php
+++ shop/backend/config/connection.php
@@ -1,16 +1,39 @@
 <?php 
-// LOCALHOST
-      // Connection with the server 
-    // MySQLie(Procedural) or PDO(Object Oriented)
-        // Server_domain, User, Password
-    $server_name = 'localhost';
-    $user_name = 'root';
-    $password = '';
-    $db_name = 'online_shop';
+/*
 
-    $conn = mysqli_connect($server_name,$user_name,$password,$db_name);
-    // Check Connection
-    if (!$conn){
-        echo 'Connection error:' . mysqli_connect_error();
-    }
+// LOCALHOST
+// Connection with the server 
+// MySQLie(Procedural) or PDO(Object Oriented)
+// Server_domain, User, Password
+$server_name = 'localhost';
+$user_name = 'root';
+$password = '';
+$db_name = 'online_shop';
+
+$conn = mysqli_connect($server_name, $user_name, $password, $db_name);
+// Check Connection
+if (!$conn) {
+    echo 'Connection error:' . mysqli_connect_error();
+}
+
+*/
+
+
+// SERVER
+
+// Connection with the server 
+// MySQLie(Procedural) or PDO(Object Oriented)
+
+$server_name = 'remotehost.es';
+$user_name = 'dwess1234';
+$password = 'Usertest1234.';
+$db_name = 'dwes';
+
+$conn = mysqli_connect($server_name, $user_name, $password, $db_name);
+
+// Check Connection
+if (!$conn) {
+    echo 'Connection error (' . mysqli_connect_errno() . '): ' . mysqli_connect_error();
+}
+
 ?>
