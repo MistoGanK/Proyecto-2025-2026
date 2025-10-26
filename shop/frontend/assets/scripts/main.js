aaaa0f72edb0d5c88d3c474f0218f5fda0e44b00
MistoGanK <nikipetkovv00@gmail.com>
2025-10-26 20:24:52 +0100

    update

--- shop/frontend/assets/scripts/main.js
+++ shop/frontend/assets/scripts/main.js
@@ -1,4 +1,35 @@
 //  Ensure content HTML elements are fits loaded
 document.addEventListener('DOMContentLoaded',(e)=>{
 
+// HTML elements
+    // Buttons
+const btnDropDown = document.getElementById('btn_drop_down');
+const btn_search = document.getElementById('btn_search');
+const btn_shopping_bag = document.getElementById('btn_shopping_bag');
+
+
+
+
+
+// Events 
+    // Buttons Events
+    btnDropDown.addEventListener('click',(e)=>{
+        
+    });
+    btn_search.addEventListener('click',(e)=>{
+
+    });
+    btn_shopping_bag.addEventListener('click',(e)=>{
+
+    });
+
+
+
+
+
+
+
+
+
+
 });
