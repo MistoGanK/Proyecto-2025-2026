d052d306b19dabb3e98a04df2ced35a050104f77
MistoGank <nikipetkovv00@gmail.com>
2025-11-03 00:17:54 +0100

    upate_11_03_2025

--- shop/frontend/assets/scripts/main.js
+++ shop/frontend/assets/scripts/main.js
@@ -1,35 +1,51 @@
 //  Ensure content HTML elements are fits loaded
 document.addEventListener('DOMContentLoaded',(e)=>{
 
-// HTML elements
-    // Buttons
-const btnDropDown = document.getElementById('btn_drop_down');
-const btn_search = document.getElementById('btn_search');
-const btn_shopping_bag = document.getElementById('btn_shopping_bag');
+//<--- HTML elements declaration --->
 
+//HTML containers
+const dropdown_menu_container = document.querySelector('.dropdown_menu_container');
+const dropdown_menu_container_backpacks = document.querySelector('.dropdown_menu_container_backpacks');
+const dropdown_menu_container_bags = document.querySelector('.dropdown_menu_container_bags');
+console.log(dropdown_menu_container_bags);
+const dropdown_menu_container_about = document.querySelector('.dropdown_menu_container_about');
 
+// HTML buttons
+const dropdown_button_show = document.getElementById('dropdown_button_show');
+const dropdown_button_close = document.getElementById('dropdown_button_close');
+const button_forward_backpacks = document.getElementById('button_forward_backpacks');
+const button_forward_bags = document.getElementById('button_forward_bags');
+console.log(button_forward_bags);
+const button_forward_about = document.getElementById('button_forward_about');
 
+//<--- Functions ---> 
+function toggleDropDownMenu(){
+    dropdown_menu_container.classList.toggle('show')
+}
+function toggleDropDownMenuBackpacks(){
+    dropdown_menu_container.classList.toggle('show');
+    dropdown_menu_container_backpacks.classList.toggle('show');
+}
+function toggleDropDownMenuBags(){
+    dropdown_menu_container.classList.toggle('show');
+    dropdown_menu_container_bags.classList.toggle('show');
+    console.log("helo");
+}
+function toggleDropDownMenuAbout(){
+    dropdown_menu_container.classList.toggle('show');
+    dropdown_menu_container_about.classList.toggle('show');
+}
+//<--- Event listeners --->
 
+// Buttons events
 
-// Events 
-    // Buttons Events
-    btnDropDown.addEventListener('click',(e)=>{
-        
-    });
-    btn_search.addEventListener('click',(e)=>{
+// Dropdown buttons
+dropdown_button_show.addEventListener('click',toggleDropDownMenu);
+dropdown_button_close.addEventListener('click',toggleDropDownMenu);
 
-    });
-    btn_shopping_bag.addEventListener('click',(e)=>{
-
-    });
-
-
-
-
-
-
-
-
+button_forward_backpacks.addEventListener('click',toggleDropDownMenuBackpacks);
+button_forward_bags.addEventListener('click',toggleDropDownMenuBags);
+button_forward_about.addEventListener('click',toggleDropDownMenuAbout);
 
 
 });
